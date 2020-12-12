0f3be7ca230e3977f6cacd3a7b2e4cb187912dfd
sashaz <sasha.zahnitko@gmail.com>
2020-12-13 09:28:50 +1000

    Just a few more small changes.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import App from "./App";
+import GlobalStyle from "./components/GlobalStyle/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ThemeProvider theme={{}}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
